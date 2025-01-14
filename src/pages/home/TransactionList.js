897677d402c53f75a2d4472f07626432ec73530d
hakan.basturknewinn.com <forxynet@gmail.com>
2025-01-14 19:11:03 +0300

    updated home page with transactionlist component

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -4,7 +4,7 @@ import styles from './Home.module.css'
 export default function TransactionList({ transactions }) {
   return (
     <ul className={styles.transactions}>
-      {transactions && transactions.map((transaction) => (
+      {transactions.map((transaction) => (
         <li key={transaction.id}>
           <p className={styles.name}>{transaction.name}</p>
           <p className={styles.amount}>${transaction.amount}</p>
