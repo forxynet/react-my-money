c22cd637ea9ad64202a58bde7875e548dd6348d0
hakan.basturknewinn.com <forxynet@gmail.com>
2025-01-14 23:37:11 +0300

    updated usefirebase hook  with delete func

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -16,6 +16,8 @@ const firestoreReducer = (state, action) => {
       return { success: false, isPending: false, error: action.payload, document: null }
     case "ADDED_DOCUMENT":
       return { success: true, isPending: false, error: null, document: action.payload }
+    case "DELETED_DOCUMENT":
+      return { success: true, isPending: false, error: null, document: action.payload }
     default:
       return state
   }
@@ -47,12 +49,18 @@ export const useFirestore = (collection) => {
     catch (err) {
       dispatchIfNotCancelled({ type: "ERROR", payload: err.message })
     }
-
   }
 
   // delete a document
-  const deleteDocument = async (doc) => {
+  const deleteDocument = async (id) => {
+    dispatch({ type: 'IS_PENDING' })
 
+    try {
+      const deltedDocument = await ref.doc(id).delete();
+      dispatchIfNotCancelled({ type: 'DELETED_DOCUMENT', payload: deltedDocument });
+    } catch (err) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: 'could not delete' })
+    }
   }
 
   useEffect(() => {
