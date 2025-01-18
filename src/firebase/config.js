9a83a50f9dee50e9e032b2bcac8a3dda977f8f8b
Hakan Baştürk <forxynet@gmail.com>
2025-01-19 02:03:20 +0300

    Update config.js
    you can put your credential

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPCSxD3CARK_pPPZ05EX6KHWrVfMENlCM",
-  authDomain: "mymoney-624a4.firebaseapp.com",
-  projectId: "mymoney-624a4",
-  storageBucket: "mymoney-624a4.firebasestorage.app",
-  messagingSenderId: "987737930649",
-  appId: "1:987737930649:web:a11783acad60d4c03ff108"
+  apiKey: ",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // init firebase
